51dae9a02f669311b9620a1afce5b3c120a6638b
Christy Serna <christyserna04@gmail.com>
2025-12-03 13:59:45 -0600

    Update and rename question4 to question4.c

--- question4.c
+++ question4.c
@@ -1,2 +1,18 @@
 //write a function void swap(int *x, int *y) that exchanges two integers by reference
 //in main, read two ints and print before AND after swapping
+
+#include <stdio.h>
+
+void swap(int *x, int*y) {
+
+
+}
+
+
+int main() {
+
+
+
+
+
+}
