5bb56345fc9074148a4fe4daf0839dbb8c61878d
Christy Serna <christyserna04@gmail.com>
2025-12-03 17:51:45 -0600

    almost done

---
+++ question11.c
@@ -0,0 +1,23 @@
+//read a string(no spaces) and compute its length without using strlen(), then print length
+
+include <stdio.h>
+
+int main() {
+
+char string[100];
+int length;
+
+printf("enter a string (no spaces) : ");
+  
+if(scanf("%99s", string) != 1) {
+printf("ERROR");
+return 1;
+}
+
+for (length = 0; string[length] != '\0'; length++) {
+}
+
+printf("the length of your string is: %d", length);
+
+  return 0;
+}
