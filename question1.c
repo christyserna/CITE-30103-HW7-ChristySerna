b4544a61262e4ef025266d00456222d1fe0b8dbb
Christy Serna <christyserna04@gmail.com>
2025-12-03 15:09:51 -0600

    question1.c DONE

--- question1.c
+++ question1.c
@@ -1,18 +1,22 @@
-//write a function float toCelsius(float f) that converts farenheit to celsius.
-//in main(), read f, call the function, and print the celsius value to two decimals
+// write a function float toCelsius(float f) that converts farenheit to celsius.
+// in main(), read f, call the function, and print the celsius value to two decimals
 
 #include <stdio.h>
 
+float toCelsius(float f) {
+return (f - 32.0) * (5.0/9.0);  //form for converting f to c
+}
+
 int main() {
 
-float farenheit, celsius;
+  float f, celsius;
 
-printf("Enter the Farenheit Temperature: ");
+  printf("Enter your temperature in Farenheit Terms: ");
+  scanf("%f", &f);
 
+  celsius = toCelsius(f);
 
-
-  
-
-}
+  printf("The conversion of your Farenheit Temperature to Celsius is %.2f",celsius);
+    
 return 0;
 }
