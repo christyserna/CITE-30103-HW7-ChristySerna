1c4afbdf2cf0e1b0b6844af29792b04cbe6e4772
Christy Serna <christyserna04@gmail.com>
2025-12-03 15:31:27 -0600

    almost done

---
+++ question3.c
@@ -0,0 +1,33 @@
+//implement int isPrime(int n), that returns 1 if n is prime, 0 otherwise.
+//in main(), read n and print Prime or 'Not Prime'
+
+
+include <stdio.h> //primes are only divided by itself and 1
+
+int isPrime(int n) {
+
+/////MEOWW
+}
+
+
+int main() {
+
+int n, result;
+
+  printf("Enter the number which you want to check is prime or not : ");
+  scanf("%d", &n);
+
+
+result = isPrime(n);
+  
+  
+if (result == 1) {
+  printf("%d IS prime!", n);
+}
+else {
+  printf("%d", is NOT prime", n);
+}
+  
+
+return 0;
+}
