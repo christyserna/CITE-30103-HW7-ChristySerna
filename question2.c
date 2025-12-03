47d7b335f2432fdd0ee92cd9714df834bdd0bb75
Christy Serna <christyserna04@gmail.com>
2025-12-03 15:21:18 -0600

    question2.c DONE

--- question2.c
+++ question2.c
@@ -1,26 +1,44 @@
-//write a function int max3(int a, int b, int c) that returns the largest of three ints WITHOUT using logical operators.
-//print result
+// write a function int max3(int a, int b, int c) that returns the largest 
+// of three ints WITHOUT using logical operators.
+// print result
 
 #include <stdio.h>
 
 int max3(int a, int b, int c) {
-largestInt = a;
-int b, c;
+int largestInt = a;
 
 
 if (b > largestInt) {
 largestInt = b;
 }
 
-else if (c > largestInt) {
+if (c > largestInt) {
 largestInt = c;
 }
 
-else {
-largestInt = a;
+return largestInt;
 }
 
-printf("The largest int is %d", largestInt)
+
+
+
+int main() {
+
+int first, second, third;
+int result;
+
+printf("Enter the first integer (a): ");
+scanf("%d", &first);
+
+printf("Enter the second integer (b): ");
+scanf("%d", &second);
+
+printf("Enter the third integer (c): ");
+scanf("%d", &third);  
+
+result = max3(first, second, third);
+  
+printf("The largest integer is %d", result);
 
 return 0;
 }
