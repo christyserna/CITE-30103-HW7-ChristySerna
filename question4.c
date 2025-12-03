c01a10fc95d91e13fcafca8b726941b31f6d3733
Christy Serna <christyserna04@gmail.com>
2025-12-03 15:41:57 -0600

    DONE

--- question4.c
+++ question4.c
@@ -4,15 +4,26 @@
 #include <stdio.h>
 
 void swap(int *x, int*y) {
-
+int meow = *x;
+*x = *y;
+*y = meow;
 
 }
 
 
 int main() {
+int first, second;
+  
+printf("what is your first integer: ");
+scanf("%d", &first);
+
+printf("what is your second integer: ");
+scanf("%d", &second);
 
 
+printf("BEFORE the swap, your values are: first = %d, second = %d", first, second);
+swap(&first, &second);
+printf("AFTER the swap, your values are: first = %d, second = %d", first, second);
 
-
-
+return 0;
 }
