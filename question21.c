ee6d4335a981aa66d9057cb074230e79952f07da
Christy Serna <christyserna04@gmail.com>
2025-12-03 22:49:45 -0600

    Update question21.c

--- question21.c
+++ question21.c
@@ -1,7 +1,7 @@
 //declare an int variable and a pointer to it.
 //print the variable's value, its address (with %p), and the dereferenced pointer's value
 
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
 
