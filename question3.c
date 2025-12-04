67c0eb2d88af16dbef01ece5f0fbfbef3ae2ddee
Christy Serna <christyserna04@gmail.com>
2025-12-03 21:08:50 -0600

    DONE

--- question3.c
+++ question3.c
@@ -2,13 +2,17 @@
 //in main(), read n and print Prime or 'Not Prime'
 
 
-include <stdio.h> //primes are only divided by itself and 1
+#include <stdio.h> //primes are only divided by itself and 1
+#include <math.h>
 
 int isPrime(int n) {
-
-/////MEOWW
+if (n <= 1 || n % 2 == 0) {
+  return 0;
 }
 
+if (n == 2) {
+  return 1;
+}
 
 int main() {
 
