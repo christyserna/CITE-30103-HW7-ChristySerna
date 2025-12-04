3665777c6f9ea4c393ecb96409c3dc2f0afdd3ea
Christy Serna <christyserna04@gmail.com>
2025-12-03 22:22:19 -0600

    Update question10.c

--- question10.c
+++ question10.c
@@ -1,3 +1,32 @@
 //read two 3x3 integer matrices, compute their sum into a third matrix, 
 // and print all three matrices clearly labeled
 
+#include <stdio.h>
+
+int main() {
+
+
+int A[3][3];
+int B[3][3];
+int C[3][3];
+
+int a, b;
+
+printf("enter elements for matrix A: ");{
+for (int i = 0; i < 3; i++) {
+printf("A[%d][%d]: ", a,b);
+scanf("%d", &A[a][b]);
+}
+}
+
+printf("enter elements for matrix B: ");{
+for (int i = 0; i < 3; i++) {
+printf("B[%d][%d]: ", a,b);
+scanf("%d", &B[a][b]);
+}
+}
+
+// ??
+  
+return 0;
+}
