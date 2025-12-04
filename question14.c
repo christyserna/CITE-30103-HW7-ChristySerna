225e83b5538a3877f45024dd57bf3fe06d4b474f
Christy Serna <christyserna04@gmail.com>
2025-12-03 18:08:13 -0600

    almost done

---
+++ question14.c
@@ -0,0 +1,42 @@
+//read two strings(no spaces) and manually cooncatenate the second to the first 
+//WITHOUT using strcat().
+//print result
+
+#include <stdio.h>
+
+int main() {
+
+char firstString[100];
+char secondString[50];
+
+printf("enter the first string: ");
+if (scanf"%s", firstString) != 1) {
+printf("ERROR");
+return 1;
+}
+
+printf("enter the second string: ");
+if (scanf("%s", secondString) != 1) {
+printf("ERROR");
+return 1;
+}
+
+int i = 0;
+while (firstString[i] != '\0') {
+i++;
+}
+
+int j = 0;
+while (secondString[j] != '\0') {
+firstString[i] = secondString[j];
+
+i++;
+j++;
+}
+
+firstString[i] = '\0';
+
+printf("the concatenated string is: %s", firstString);
+
+return 0;
+}
