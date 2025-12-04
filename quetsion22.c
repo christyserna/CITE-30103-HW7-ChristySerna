fd1e9ad6f1fb63fb40d5b145bf7408bbb3aa2ec0
Christy Serna <christyserna04@gmail.com>
2025-12-03 20:19:35 -0600

    almost done

---
+++ quetsion22.c
@@ -0,0 +1,28 @@
+// input 5 integers into an array and print them using pointer arithmetic only (no [] indexing)
+
+include <stdio.h>
+
+int main() {
+
+int arr[5];
+int size = 5;
+int i;
+
+printf("enter your five integers: ");
+
+for (i = 0; i < size; i++) {
+printf("element %d: ", i + 1);
+
+if (scanf("%d", (arr + i )) != 1) {
+printf("ERROR");
+return 1;
+}
+
+printf("you entered: ");
+
+for (i = 0; i < size; i++) {
+printf("element %d: %d", i + 1, *(arr + i));
+}
+}
+return 0;
+}
