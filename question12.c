81dcf0412891e7c73f6304657b92dd8cd9466bdc
Christy Serna <christyserna04@gmail.com>
2025-12-03 21:41:08 -0600

    DONE

--- question12.c
+++ question12.c
@@ -7,8 +7,31 @@
 
 int main() {
 
+int start = 0;
+int end;
 
+  int palindrome = 1;
 
+  printf("enter a word(no whitespace): ");
+  if (scanf("%d", word) != 1 {
+    printf("ERROR");
+    return 1;
+  }
+  end = strlen(word) - 1;
+
+  while (start < end) {
+    if (word[start] != word[end]) {
+      palindrome = 0;
+      break;
+    }
+    start++
+    end++
+
+      if (palindrome == 1 {
+      printf("the word you entered IS a palindrome");
+      else {
+        printf("the word you entered IS NOT  a palindrome");
+      }
   
 
 return 0;
