44c3eff5502c71fc2dca43ce12ffa80546e359aa
Christy Serna <christyserna04@gmail.com>
2025-12-03 23:19:24 -0600

    Update question19.c

--- question19.c
+++ question19.c
@@ -1,4 +1,5 @@
-//add a nested structure struct Date { int day, int month; int year; }; inside Student for date of birth.
+//add a nested structure struct Date { int day, int month; int year; }; inside Student
+//for date of birth.
 //read one student and print all fields including DOB
 
 
