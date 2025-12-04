1dcefb3a0964b1325c36cb30c556952e6715f631
Christy Serna <christyserna04@gmail.com>
2025-12-03 22:49:04 -0600

    Update question15.c

--- question15.c
+++ question15.c
@@ -1,7 +1,7 @@
 //read a line of text up to 100 chars. counthe number of words, 
 // considering words separated by one or more spaces. print result.
 
-include <stdio.h>
+#include <stdio.h>
 
 int main() {
 
