e5f945509db05e2d0fd155a03a0d18be03e00b86
Christy Serna <christyserna04@gmail.com>
2025-12-03 19:55:54 -0600

    almost done

---
+++ question20.c
@@ -0,0 +1,33 @@
+//write a function void printStudent(struct Student s) that prints the 
+//fields of a passed-in Student/ in main(), populate one Student and call the function
+//idk what it means by populate im ngl
+
+include <stdio.h>
+#include <string.h>
+
+struct Student {
+char name[50];
+char roll;
+float marks;
+};
+
+void printStudent(struct Student s) {
+printf("the students name is: %s", s.name);
+printf("the students roll is: %c", s.roll);
+printf("the students marks is: %f", s.marks);
+}
+
+int main() {
+
+  struct Student firstStudent;
+
+  strcpy(firstStudent.name, "Pusheen The Cat");
+  firstStudent.roll = 'A';
+  firstStudent.marks = 99.0;
+
+  
+  printStudent(firstStudent);
+
+
+return 0;
+}
