ee8c909d621befe4a31689a760bc3018fa04986b
Christy Serna <christyserna04@gmail.com>
2025-12-03 18:10:01 -0600

    unfinished

---
+++ question15.c
@@ -0,0 +1,12 @@
+//read a line of text up to 100 chars. counthe number of words, 
+// considering words separated by one or more spaces. print result.
+
+include <stdio.h>
+
+int main() {
+
+
+
+
+return 0;
+}
